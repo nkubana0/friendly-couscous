e33752de8a426c4287b1c6e67542508f239aac6f
nkubana0 <i.shema@alustudent.com>
2024-04-15 16:05:39 +0200

    buttons and alerts

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,12 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import Alert from "./components/alert";
+import Button from "./components/button";
+
 function App() {
-  return (
-    <div>
-      <ListGroup />
-    </div>
-  );
+  const [alertVisible, setAlertVisibility] = useState(false);
+  return <div>
+    {alertVisible && <Alert onClose={()=> setAlertVisibility(false)}>My Alert</Alert>}
+    <Button color="danger" onClick={() => setAlertVisibility(true)}>My Button</Button>
+  </div>
 }
 export default App;
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,18 +1,26 @@
 import { useState } from "react";
 
-function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem : (item:string) => void;
+}
+
+function ListGroup({items, heading, onSelectItem}: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No Items found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li
             className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
             key={item}
-            onClick={() => setSelectedIndex(index)}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
